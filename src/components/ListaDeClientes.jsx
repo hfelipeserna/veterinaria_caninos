9120c389a6b7d048d50773529fafed895250369d
Hernán Felipe Serna Gómez <hfserna@gmail.com>
2021-09-09 19:43:14 -0500

    [REFACTOR] Agregar componentes ListaDeClientes.jsx y Perro.jsx

--- src/components/ListaDeClientes.jsx
+++ src/components/ListaDeClientes.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import Perro from './Perro';
+import data from '../data/data.json';
 
 class ListaDeClientes extends React.Component {
-
+  render(){
+    return(
+      <div>
+        <h1>Listado de pacientes</h1>
+        <ul style={{listStyle:"none"}}>
+          {
+            data.map((perro, index) =>(
+              <React.Fragment>
+                <Perro id={index} datos={perro}/>
+              </React.Fragment>  
+            ))
+          }
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default ListaDeClientes;
---
+++ src/components/Perro.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Perro extends React.Component {
+  render() {
+      const data = this.props.datos;
+    return (
+    <React.Fragment>
+        <h2>Nombre: {data.nombre}</h2>
+        <ul style={{listStyle:"none"}}>
+            <li>Edad: {data.edad}</li>
+            <li>Sexo: {data.sexo}</li>
+            <li>Raza: {data.raza}</li>
+            <li>Tamaño: {data.tamaño}</li>
+        </ul>
+    </React.Fragment>
+    );
+  }
+}
+
+export default Perro;
